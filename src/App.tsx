9e45514d9fd8b244b30e8a849f30671c926e4b6a
1carlosrenan <carlosrenan96@gmail.com>
2023-03-07 19:20:54 -0300

    feat: adding button to like and dislike

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
 import useGithubRepositories from "./hooks/useGithubRepositories";
 
+type RepositoryState = {
+  id: number;
+  liked: boolean;
+}
+
 function App() {
-  const { repos, isLoading, error } = useGithubRepositories({ org: 'google' })
+  const { repos, isLoading, error } = useGithubRepositories({ org: 'google' });
+  const [repositoryLikes, setRepositoryLikes] = useState<RepositoryState[]>([]);
+
+  function toggleLike(id: number) {
+    setRepositoryLikes(prevState => {
+      const exists = prevState.find((r) => r.id === id)
+
+
+      if (exists) {
+        return prevState.map((r) =>
+          r.id === id ? { ...r, liked: !r.liked } : r
+        );
+      }
+
+      return [...prevState, { id, liked: true }]
+    });
+  }
 
   return (
     <div>
       {isLoading && <p>Carregando...</p>}
       {error && <p>Erro ao carregar dados</p>}
       {repos.map((repo) => {
+        const isLiked = repositoryLikes.find((r) => r.id === repo.id)?.liked;
         return (
           <div key={repo.id}>
             <h2>
               {repo.full_name}
               <span> by {repo.owner.login}</span>
-              <button> Like </button>
+              <button onClick={() => toggleLike(repo.id)}>
+                {isLiked ? "Descurtir" : "Curtir"}
+              </button>
             </h2>
           </div>
         )
