b59edd8c95da289b5f028530901240b80a701707
1carlosrenan <carlosrenan96@gmail.com>
2023-03-06 21:23:40 -0300

    feat: adding fetch datas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+
+interface IRepository {
+  id: number;
+  full_name: string;
+  owner: {
+    login: string;
+  }
+}
+
 function App() {
-  return <div className="App">teste</div>;
+  const [repos, setRepos] = useState<IRepository[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/google/repos")
+      .then((r) => r.json())
+      .then((data) => {
+        setRepos(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {repos.map((repo) => {
+        return (
+          <div key={repo.id}>
+            <h2>
+              {repo.full_name}
+              <span> by {repo.owner.login}</span>
+            </h2>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default App;
